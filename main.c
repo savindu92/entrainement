4073b63b04e109ed192fe27f46c80b5f370781b5
savindu92 <savindu.edirisuriya_mohottige_de_saram@edu.devinci.fr>
2021-12-11 16:47:36 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+int main() { 
+	prinft("1\n");
+	prinft("2\n");
+	prinft("3\n");
+	prinft("4\n");
+	prinft("5\n");	
+	prinft("6\n");
+	prinft("7\n");
+	prinft("8\n");
+	prinft("9\n");
+	prinft("10\n");
+	return 0;
+
+} 
+
