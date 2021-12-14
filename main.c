cf0ef022a7eb740c707ca6cec8ca03ef939af0fd
EDIRISURIYASavindu <94961795+savindu92@users.noreply.github.com>
2021-12-14 15:47:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+##include <stdio.h>
 #include <stdlib.h>
 
 int result;
@@ -10,14 +10,14 @@ int number_to_guess;
 scanf("%d",&number_to_guess);
 
 while(number_to_guess != result){
-if(number_to_guess < result){
-printf("The number to guess is higher");
-}
-else if(number_to_guess > result){
-printf("The number to guess is lower");
-}
-else if(number_to_guess = result){
-printf("You just found the number , it was indeed %d", result);
-}
+	if(number_to_guess < result){
+	printf("The number to guess is higher");
+	}
+	else if(number_to_guess > result){
+	printf("The number to guess is lower");
+	}
+	else if(number_to_guess == result){
+	printf("You just found the number , it was indeed %d", result);
+	}
 }
 return 0;
