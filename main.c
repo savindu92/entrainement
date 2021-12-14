5b592af332f4bc9e30a1215a7e93ceea4ebdee02
EDIRISURIYASavindu <94961795+savindu92@users.noreply.github.com>
2021-12-14 16:02:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
-##include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int result;
-result = rand() % 100 + 1;
-
-printf("I have in mind a number in between 1 and 100, can you find it ?");
-
-int number_to_guess;
-scanf("%d",&number_to_guess);
-
-while(number_to_guess != result){
-	if(number_to_guess < result){
-	printf("The number to guess is higher");
+int main(){
+        
+        int result = rand() % 100 + 1;
+        int guess = 0;
+        printf("I have in mind a number in between 1 and 100, can you find it?\n");
+        while (guess != result){
+                scanf("%d", &guess);
+                if(guess >= 1 && guess <= 100){
+                        if (guess > result){
+			printf("The number to guess is lower\n");
+		}
+			else if (result > guess){
+				printf("The number to guess is higher\n");
+			}
+		}
+		else{
+			printf("Erreur\n");
+			return 1;
+		}
 	}
-	else if(number_to_guess > result){
-	printf("The number to guess is lower");
-	}
-	else if(number_to_guess == result){
-	printf("You just found the number , it was indeed %d", result);
-	}
+	printf("You just found the number, it was indeed %d\n", guess);
+	return 0;
 }
-return 0;
