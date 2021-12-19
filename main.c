30610d7cb1a959202b69f74a7677825707fddc03
EDIRISURIYASavindu <94961795+savindu92@users.noreply.github.com>
2021-12-19 16:34:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
-#include <stdio.h>			 	  				   	     
-#include <string.h>			 	  				   	     
-			 	  				   	     
-int main(){			 	  				   	     
-    char str[100];			 	  				   	     
-    scanf("%s", str);			 	  				   	     
-    printf("Bonjour %s", str);			 	  				   	     
-			 	  				   	     
-			 	  				   	     
-    return 0;			 	  				   	     
-}			 	  				   	     
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello world");
+    
+    return 0;
+}
